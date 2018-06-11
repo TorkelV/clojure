0d26c82f0897965c4570dc46a676efb24c142724
Torkel Velure <torkel93@gmail.com>
2018-06-11 05:47:01 +0200

    ...

--- src/finn/core.clj
+++ src/finn/core.clj
@@ -11,22 +11,43 @@
 (defn app []
   (routes
     (GET "/" [:as req]
-         {:status 200
-          :headers {"Content-Type" "text/html" }
-          :body "<h1>hello</h1>"})
+      {:status  200
+       :headers {"Content-Type" "text/html"}
+       :body    "<h1>hello</h1>"})
     (GET "/:user-name" [user-name :as req]
-         {:status 200
-          :headers {"Content-Type" "text/html"}
-          :body (format "<h1> hi from %s" user-name)})))
+      {:status  200
+       :headers {"Content-Type" "text/html"}
+       :body    (format "<h1> hi from %s" user-name)})))
 
 (defn create-server []
-   (server/run-server (app) {:port 8080}))
+  (server/run-server (app) {:port 8080}))
 
 (defn run-server []
   (reset! server (create-server)))
 
 (defn stop-server [server]
   (server :timeout 100))
+
+
+(defn merge-arrays-to-map [ks vs]
+  (loop [res '{} keys ks vals vs]
+    (if (= 0 (count vals))
+      res
+      (recur (assoc res (first keys) (first vals)) (drop 1 keys) (drop 1 vals))
+      )
+    )
+  )
+
+(defn hickmaptostring [items]
+  (clojure.string/join "\n"
+                       (flatten
+                         (remove nil?
+                                 (loop [col (flatten items) cnt 30]
+                                   (if (and (> cnt 0) (some #(= (type %) clojure.lang.PersistentArrayMap) (flatten col)))
+                                     (recur (map #(if (= (type %) clojure.lang.PersistentArrayMap) (:content %) %) (flatten col)) (dec cnt))
+                                     (if (<= cnt 1) "" col))))))
+  )
+
 
 
 (defn hitcount [body]
@@ -58,11 +79,11 @@
 (defn itannonser []
   (def url "https://www.finn.no/job/fulltime/search.html?location=0.20001&occupation=0.23&page=")
   (let [body (htmlbody (str url "1"))]
-      (loop [res '() cnt (pages body)]
-        (if (<= cnt 1.0)
-          res
-          (let [tmpa (htmlbody (str url cnt))]
-            (recur (concat res (annonser tmpa)) (dec cnt))))))
+    (loop [res '() cnt (pages body)]
+      (if (<= cnt 1.0)
+        res
+        (let [tmpa (htmlbody (str url cnt))]
+          (recur (concat res (annonser tmpa)) (dec cnt))))))
   )
 
 
@@ -74,11 +95,26 @@
                                    (s/class "object-description")) body)
                                first
                                :content)))]
-    (clojure.string/join "\n"
-                         (flatten
-                           (remove nil?
-                                   (loop [col (flatten items) cnt 30]
-                                     (if (and (> cnt 0) (some #(= (type %) clojure.lang.PersistentArrayMap) (flatten col)))
-                                       (recur (map #(if (= (type %) clojure.lang.PersistentArrayMap) (:content %) % ) (flatten col)) (dec cnt))
-                                       col)))))
-    ))
+    (hickmaptostring items)))
+
+(defn tittel [body]
+  (-> (s/select (s/descendant (s/class "h1")) body)
+      first
+      :content
+      first))
+
+(defn semi-strukturert-beskrivelse [body]
+  (let [grouped (map (fn [k] (partition-by #(get (get % :attrs) :data-automation-id) k))
+                     (map #(remove string? %)
+                          (map :content
+                               (-> (s/select (s/descendant (s/class "r-prl")) body)))))]
+    (let [g2 (map flatten
+                  (apply concat
+                         (map (fn [v]
+                                (partition 2
+                                           (map #(map :content %) v)))
+                              grouped)))]  (map (fn [v] (map #(if (= (type %) clojure.lang.PersistentArrayMap) (:content %) %) v) ) g2)) )
+  )
+
+
+
